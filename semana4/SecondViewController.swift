48de3b875751ffae0d6674400388d43a713fa158
linder3hs <linderhassinger00@gmail.com>
2021-09-07 10:22:33 -0500

    first commit :alien:

---
+++ semana4/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  semana4
+//
+//  Created by Linder Hassinger on 7/09/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - Navigation
+
+}
--- semana4/ViewController.swift
+++ semana4/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var iconButtonMobile: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        iconButtonMobile.image = UIImage(named: "mobile")?.withRenderingMode(.alwaysOriginal)
+
+        print("mi pantalla inicio")
     }
-
-
+    
+//    override func loadView() {
+//        print("mi load view")
+//    }
+    
+    // Antes de que carguen los objetos UI
+    override func viewWillAppear(_ animated: Bool) {
+        print("view will appear")
+    }
+    
+    // Despues de que carguen los objetos UI
+    override func viewDidAppear(_ animated: Bool) {
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("view did disappear")
+    }
+    
 }
 
